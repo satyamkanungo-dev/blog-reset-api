eabce23e45a01d1325a24c9e2d8927fe895e3256
satyamkanungo-dev <satyamkanungo0@gmail.com>
2026-06-30 11:30:45 +0530

    add: db struct tags

--- internal/models/blog.go
+++ internal/models/blog.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type Blog struct {
-	Id        string    `json:"id"`
-	UserId    string    `json:"user_id"`
-	Title     string    `json:"title"`
-	Content   string    `json:"content"`
-	Category  string    `json:"category"`
-	Tags      []string  `json:"tags"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	Id        string    `json:"id" db:"Id"`
+	UserId    string    `json:"user_id" db:"UserId"`
+	Title     string    `json:"title" db:"Title"`
+	Content   string    `json:"content" db:"Content"`
+	Category  string    `json:"category" db:"Category"`
+	Tags      []string  `json:"tags" db:"Tags"`
+	CreatedAt time.Time `json:"created_at" db:"CreatedAt"`
+	UpdatedAt time.Time `json:"updated_at" db:"UpdatedAt"`
 }
