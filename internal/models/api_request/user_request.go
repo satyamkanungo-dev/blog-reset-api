722c8abe7f8bd91ed33e94e1ff2eea0845eceb61
satyamkanungo-dev <satyamkanungo0@gmail.com>
2026-06-30 20:41:10 +0530

    add(models): user request

---
+++ internal/models/api_request/user_request.go
@@ -0,0 +1,12 @@
+package apirequest
+
+type RegisterRequest struct {
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
