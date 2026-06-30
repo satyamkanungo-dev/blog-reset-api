5a3fea9569bd7c9e05f67c7019973e736ad67943
satyamkanungo-dev <satyamkanungo0@gmail.com>
2026-06-30 16:11:40 +0530

    Implement server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/gin-gonic/gin"
 	"github.com/satyamkanungo-dev/blog-rest-api/internal/config"
 	"github.com/satyamkanungo-dev/blog-rest-api/internal/database"
+	"github.com/satyamkanungo-dev/blog-rest-api/internal/server"
 )
 
 func main() {
@@ -26,5 +32,29 @@ func main() {
 
 	defer pool.Close()
 
-	fmt.Println("Everything is ok")
+	// eg:
+	router := gin.Default()
+	router.GET("api/v1", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Blog API 👍",
+		})
+	})
+
+	workDone := make(chan os.Signal, 1)
+
+	svr := server.InitializeServer(router, cfg.Port)
+	go func() {
+		if err := svr.Serve(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("failed to start server: %v", err)
+		}
+	}()
+
+	signal.Notify(workDone, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	<-workDone
+	slog.Info("shutting down server...")
+
+	if err := svr.ShutDown(); err != nil {
+		slog.Error("graceful shutdown failed", "error:", err)
+	}
 }
