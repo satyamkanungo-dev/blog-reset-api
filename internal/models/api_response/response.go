cfc985ad7f3b1f492332dc516ba5b6fe0bc17ba9
satyamkanungo-dev <satyamkanungo0@gmail.com>
2026-07-01 10:59:58 +0530

    feat: add blogs response

--- internal/models/api_response/response.go
+++ internal/models/api_response/response.go
@@ -1,5 +1,8 @@
 package apiresponse
 
+import models "github.com/satyamkanungo-dev/blog-rest-api/internal/models/core"
+
+// core
 type APIResponse struct {
 	Code    int    `json:"code"`   // http status
 	Status  string `json:"status"` // success or error
@@ -7,6 +10,7 @@ type APIResponse struct {
 	Data    any    `json:"data,omitempty"`
 }
 
+// user
 type UserResponse struct {
 	Id       string `json:"id"`
 	UserName string `json:"username"`
@@ -17,4 +21,11 @@ type AuthResponse struct {
 	AccessToken  string       `json:"access_token"`
 	RefreshToken string       `json:"refresh_token"`
 	User         UserResponse `json:"user"`
+}
+
+// blog
+type BlogsResponse struct {
+	Data       []models.Blog `json:"data"`
+	NextCursor any           `json:"next_cursor"`
+	HasNext    bool          `json:"has_next"`
 }
