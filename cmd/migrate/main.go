36db4558d63bff7117370725209846b986727d62
satyamkanungo-dev <satyamkanungo0@gmail.com>
2026-07-02 21:18:35 +0530

    feat: Add database migration CLI using golang-migrate with PostgreSQL

---
+++ cmd/migrate/main.go
@@ -0,0 +1,66 @@
+package migrate
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/database/postgres"
+	"github.com/golang-migrate/migrate/source/file"
+	"github.com/satyamkanungo-dev/blog-rest-api/internal/config"
+)
+
+const migrationPath = "../../migrations/"
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide a migration direction: 'up' or 'down'")
+	}
+
+	direction := os.Args[1]
+
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = cfg.Validate(); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := sql.Open("postgres", cfg.DatabaseURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	instance, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fSrc, err := (&file.File{}).Open(migrationPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithInstance("file", fSrc, "postgres", instance)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch direction {
+	case "up":
+		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	case "down":
+		if err := m.Down(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	default:
+		log.Fatal("Invalid direction. Use 'up' or 'down'")
+	}
+
+}
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 )
 
 require (
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.14.3 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -18,6 +19,7 @@ require (
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/lib/pq v1.10.2 // indirect
 	golang.org/x/crypto v0.20.0 // indirect
 	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/text v0.29.0 // indirect
